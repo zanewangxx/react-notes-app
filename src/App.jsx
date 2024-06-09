9b77f3bd35474e0047d1a92f945f3fda530fdb60
Zhen Wang <wangzhenaph@gmail.com>
2024-06-09 22:56:57 +1000

    try fix the issue, app shows only a black screen now

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Notes from "./pages/Notes"
-import CreateNote from "./pages/CreateNote"
-import EditNote from "./pages/EditNote"
-import { useState, useEffect } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import CreateNote from "./pages/CreateNote";
+import EditNote from "./pages/EditNote";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
-  //console.log(notes);
+
   useEffect(() => {
-    localStorage.setItem("notes", JSON.stringify(notes))
+    localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
 
   return (
     <main id="app">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Notes notes={notes}/>}/>
           <Route path="/create-note" element={<CreateNote setNotes={setNotes}/>}/>
           <Route path="/edit-note/:id" element={<EditNote notes={notes} setNotes={setNotes}/>}/>
-
         </Routes>
       </BrowserRouter>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
